2d21dcb12ac826077d24275b97d811026d192091
Marvin Hohbach <marvin.hohbach@datev.de>
2020-02-11 12:13:00 +0000

    Apply suggestion to index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ module.exports = (options) => ({
 
     define: {
         SEARCH_MAX_SUGGESTIONS: options.searchMaxSuggestions || 5,
-        SEARCH_PATHS: options.test || null,
         SEARCH_HOTKEYS: options.searchHotkeys || ['s', '/'],
         DOCUMENTS: options.documents,
         ACTIVATE_STEMMING: options.activateStemming || false,
